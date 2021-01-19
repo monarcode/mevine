f1d4caa0d6f78e8de28caec8299df97a24acad90
Ugbah Isioma <61241222+monarcode@users.noreply.github.com>
2021-01-19 12:10:14 +0100

    init

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    // $('.owl-carousel').owlCarousel({
+    //     loop:false,
+    //     animateOut: 'slideOutDown',
+    //     animateIn: 'flipInX',
+    //     margin:10,
+    //     responsiveClass:true,
+    //     responsive:{
+    //         0:{
+    //             items:1,
+    //             nav:false
+    //         },
+    //         600:{
+    //             items:2,
+    //             nav:false
+    //         }
+    //     }
+    // })
+})
